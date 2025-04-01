c498a61b83d554ca3451913a647cec0b4df736b5
QuantumD3v <shapebrabra@gmail.com>
2025-04-02 04:28:54 +0700

    Updated index.js

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const port = 3001;
 // Serve static files (CSS, JS, etc.) from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Files route
-app.get('/files', (req, res) => {
-  const directoryPath = path.join(__dirname, 'public');
+app.get('/css', (req, res) => {
+  const directoryPath = path.join(__dirname, 'public', 'css');
   fs.readdir(directoryPath, (err, files) => {
     if (err) {
       res.status(500).send('Unable to scan directory');
@@ -16,6 +15,47 @@ app.get('/files', (req, res) => {
     }
     res.send(files);
   });
+});
+
+app.get('/js', (req, res) => {
+  const directoryPath = path.join(__dirname, 'public', 'js');
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      res.status(500).send('Unable to scan directory');
+      return;
+    }
+    res.send(files);
+  });
+});
+
+app.get('/sudo', (req, res) => {
+  const directoryPath = path.join(__dirname, 'private');
+  const encodedUser = req.query.u;
+
+  if (!encodedUser) {
+    return res.status(400).send('Missing user parameter');
+  }
+
+  try {
+    // Decode the Base64-encoded user
+    const decodedUser = Buffer.from(encodedUser, 'base64').toString();
+
+    // Check if the decoded user is allowed
+    if (decodedUser !== 'ccelestia') {
+      return res.status(403).send('You are not authorized to access this directory');
+    }
+
+    // Read the directory if the user is authorized
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        return res.status(500).send('Unable to scan directory');
+      }
+      res.send(files);
+    });
+  } catch (error) {
+    // Handle invalid Base64 strings
+    return res.status(400).send('Invalid Base64-encoded user parameter');
+  }
 });
 
 // Default Route
